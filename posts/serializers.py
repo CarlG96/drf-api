fb6a298a3fd36b450600d0a52a734852cb9cb5f8
CarlG96 <carl.gardiner1@hotmail.com>
2022-10-27 15:33:04 +0000

    Added image filtering

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
 from .models import Post
 
+
 class ProfileSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField()
+    owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializer.ReadOnlyField(source='owner.profile.id')
     profile_image = serializer.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'image size larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -16,5 +32,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title',
             'content', 'image', 'owmer', 'is_owner',
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_image', 'image_filter'
         ]
