f30c8fbca180fbea6238a15176c6382acfe199ea
CarlG96 <carl.gardiner1@hotmail.com>
2022-11-04 13:20:08 +0000

    for deploy

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.envron
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     'moments-drf-api-carl.herokuapp.com',
