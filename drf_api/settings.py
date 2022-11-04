7220a7687d355318eb4a85d30234eab657595166
CarlG96 <carl.gardiner1@hotmail.com>
2022-11-04 13:28:01 +0000

    changes

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -147,7 +147,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABSE_URL')
+        os.environ.get('DATABASE_URL')
     )
 }
 
